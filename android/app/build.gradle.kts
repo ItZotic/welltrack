6322a4d4806065826b453b4006c16b9a88bdcad6
ItZotic <jm.pimentel2101@gmail.com>
2025-11-25 14:59:51 +0800

    2nd push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
@@ -39,6 +40,11 @@ android {
     }
 }
 
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+}
 flutter {
     source = "../.."
 }
